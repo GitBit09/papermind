0ee46badb616a55f53ed64d621899c62351ba90d
Arpita121496 <arpitakumari2209@gmail.com>
2026-03-28 13:07:42 +0530

    Add Procfile for deployment

--- app.py
+++ app.py
@@ -404,4 +404,4 @@ def build_visual_html(vtype, vtitle, vdata):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
