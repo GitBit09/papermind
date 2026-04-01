90c72c4a14a06527c6ad1736d6bf32412533d31c
Arpita121496 <arpitakumari2209@gmail.com>
2026-04-01 11:32:57 +0530

    fix railway deployment

--- app.py
+++ app.py
@@ -404,4 +404,5 @@ def build_visual_html(vtype, vtitle, vdata):
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
